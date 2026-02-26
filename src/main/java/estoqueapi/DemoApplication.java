8f33cdc5a56e33fadfbfa5e2367ef55e5e9c7c1e
Ruan fontes <ruanfontes309@gmail.com>
2026-02-26 15:03:34 -0300

    inicializando commit - Spring boot Estoque API

---
+++ src/main/java/estoqueapi/DemoApplication.java
@@ -0,0 +1,13 @@
+package estoqueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estoqueapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package estoqueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
